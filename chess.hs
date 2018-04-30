951b586270e0f7cca45429e42de425160b3a0ef2
Sebastiaan de Kwant <basdekwant@gmail.com>
2018-04-30 07:30:27 +0200

    Some ideas for diagonals

--- chess.hs
+++ chess.hs
@@ -1,15 +1,15 @@
 module Chess
-(
-  legalMoves,
-  consPos,
-  PieceInstance
-) where
+  (
+    legalMoves,
+    consPos,
+    PieceInstance
+  ) where
 
 import Data.Maybe
 
 data Color = Black | White deriving (Show, Eq)
 
-data Piece = Rook | Bisshop | King deriving (Show, Eq)
+data Piece = Rook | Bisshop | King | Queen deriving (Show, Eq)
 
 data PieceInstance = PieceInstance Color Piece deriving (Show, Eq)
 
@@ -23,16 +23,17 @@ constructablePos x y = 0 < x && x <= 8 && 0 < y && y <= 8
 
 consPosM :: Int -> Int -> Maybe Position
 consPosM x y | (constructablePos x y) = Just $ Position x y
-             | otherwise = Nothing
+            | otherwise = Nothing
 
 consPos :: Int -> Int -> Position
 consPos x y | (constructablePos x y) = Position x y
             | otherwise = error "Out of reach"
 
 humanReadablePosition :: Position -> (Char, Int)
-humanReadablePosition (Position x y) = let letters = ['a'..'h']
-                                           letter = letters !! (x + 1)
-                                       in (letter, y)
+humanReadablePosition (Position x y) = let letters = ['A'..'H']
+                                           letter = letters !! (x - 1)
+                                        in (letter, y)
+
 
 
 diagonalAdder :: Int -> Int -> Maybe Position
@@ -53,12 +54,24 @@ legalMoves Rook pos = let x = h pos
                           moves = filter (\a -> a /= pos) $ catMaybes $ hRange ++ vRange
                       in moves
 
-legalMoves King pos = let x = h pos
-                          y = v pos
-                          range = [(-1)..1]
-                          hRange = map (consPosM x) $ map (\n -> n + y) range
-                          vRange = map (consPosM y) $ map (\n -> n + x) range
-                          moves = []
-                      in moves
+dfRange :: [Maybe Position] -> [Maybe Position]
+dfRange moves = let nextBase = head moves
+                    xBase = fromJust $ fmap h nextBase
+                    yBase = fromJust $ fmap v nextBase
+                    next = diagonalAdder xBase yBase
+                    moves' = next : moves
+                in dfRange (moves')
+
+
+
+legalMoves' Queen pos = let x = h pos
+                            y = v pos
+                            straightRange = [(-8)..8]
+                            hRange = map (consPosM x) $ map (\n -> n + y) straightRange
+                            vRange = map (consPosM y) $ map (\n -> n + x) straightRange
+                            dRange = 8
+                            dfMoves = [Just pos]
+                            moves = vRange
+                      in catMaybes moves
 
 
