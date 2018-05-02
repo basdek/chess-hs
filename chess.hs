b64402474adee563c196a50aff0e5136d1d264d0
Sebastiaan de Kwant <basdekwant@gmail.com>
2018-05-02 23:58:30 +0200

    Adding legalmoves
    (Pawn is not complete, taking an enemy piece is not done yet)

--- chess.hs
+++ chess.hs
@@ -9,7 +9,7 @@ import Data.Maybe
 
 data Color = Black | White deriving (Show, Eq)
 
-data Piece = Rook | Bisshop | King | Queen deriving (Show, Eq)
+data Piece = Rook | Bisshop | King | Queen | Knight | Pawn deriving (Show, Eq)
 
 data PieceInstance = PieceInstance Color Piece deriving (Show, Eq)
 
@@ -34,44 +34,59 @@ humanReadablePosition (Position x y) = let letters = ['A'..'H']
                                            letter = letters !! (x - 1)
                                         in (letter, y)
 
+--Helper creating a diagonal position from an x, y coordinate.
+diagonalAdder :: Int -> Int -> Int -> Maybe Position
+diagonalAdder x y n  =  consPosM (x + n) (y + n)
 
+fullRange = [(-8)..8]
 
-diagonalAdder :: Int -> Int -> Maybe Position
-diagonalAdder x y = consPosM (x + 1) (y + 1)
+data Board = Board
 
-diagonalSubtr :: Int -> Int -> Maybe Position
-diagonalSubtr x y = consPosM (x - 1) (y - 1)
+check :: Board -> Bool
+check _ = False
 
 
 legalMoves :: Piece -> Position -> [Position]
+
 legalMoves Rook pos = let x = h pos
                           y = v pos
-                          range = [(-8)..8]
-                          vRange = map (consPosM x) $ map (\n -> n + y) range
-                          hRange = map (flip consPosM y) $ map (\n -> n + x) range
+                          vRange = map (consPosM x) $ map (\n -> n + y) fullRange
+                          hRange = map (flip consPosM y) $ map (\n -> n + x) fullRange
 
-                          --Get only on the grid moves, filter out the starting position
+                          --Get only on the grid moves, filter out the starting position.
                           moves = filter (\a -> a /= pos) $ catMaybes $ hRange ++ vRange
                       in moves
 
-dfRange :: [Maybe Position] -> [Maybe Position]
-dfRange moves = let nextBase = head moves
-                    xBase = fromJust $ fmap h nextBase
-                    yBase = fromJust $ fmap v nextBase
-                    next = diagonalAdder xBase yBase
-                    moves' = next : moves
-                in dfRange (moves')
-
-
-
-legalMoves' Queen pos = let x = h pos
+legalMoves Queen pos = let  x = h pos
                             y = v pos
-                            straightRange = [(-8)..8]
-                            hRange = map (consPosM x) $ map (\n -> n + y) straightRange
-                            vRange = map (consPosM y) $ map (\n -> n + x) straightRange
-                            dRange = 8
-                            dfMoves = [Just pos]
-                            moves = vRange
-                      in catMaybes moves
+                            hRange = map (consPosM x) $ map (\n -> n + y) fullRange
+                            vRange = map (flip consPosM y) $ map (\n -> n + x) fullRange
+                            dRange = map (diagonalAdder x y) fullRange
 
+                            --Get only on the grid moves (horizontal, vertical, diagonal),
+                            --filter out starting position.
+                            moves = filter (\a -> a /= pos) $ catMaybes $ vRange ++ hRange ++ dRange
+                       in moves
 
+legalMoves Bisshop pos = let x = h pos
+                             y = v pos
+                             --Get only on the grid moves, filter out the starting position.
+                             moves = filter (\a -> a /= pos) $ catMaybes $ map (diagonalAdder x y) fullRange
+                         in moves
+
+legalMoves King pos = let x = h pos
+                          y = v pos
+                          range1 = [(-1)..1]
+                          hRange = map (consPosM x) $ map (\n -> n + y) range1
+                          vRange = map (flip consPosM y) $ map (\n -> n + x) range1
+                          dRange = map (diagonalAdder x y) range1
+                          --Get only on the grid moves, filter out the starting position.
+                          moves = filter (\a -> a /= pos) $ catMaybes $ hRange ++ vRange ++ dRange
+                       in moves
+
+--Handling a start position for white.
+legalMoves Pawn (Position x 2) = [Position x 3, Position x 4]
+--Handling a start position for black.
+legalMoves Pawn (Position x 7) = [Position x 6, Position x 5]
+--Handling all non starting positions.
+legalMoves Pawn (Position x y) = [Position x (y+1)]
