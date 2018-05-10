ce58500f3f7cdf386563d03789055ea063e97d9c
Sebastiaan de Kwant <basdekwant@gmail.com>
2018-05-11 00:26:01 +0200

    Add knight! Minor visual edits
    Minor things:
    * Reordering the _Ranges alphabetically.
    * An indentation fix

--- chess.hs
+++ chess.hs
@@ -6,6 +6,7 @@ module Chess
   ) where
 
 import Data.Maybe
+import Data.Tuple
 
 data Color = Black | White deriving (Show, Eq)
 
@@ -23,7 +24,7 @@ constructablePos x y = 0 < x && x <= 8 && 0 < y && y <= 8
 
 consPosM :: Int -> Int -> Maybe Position
 consPosM x y | (constructablePos x y) = Just $ Position x y
-            | otherwise = Nothing
+             | otherwise = Nothing
 
 consPos :: Int -> Int -> Position
 consPos x y | (constructablePos x y) = Position x y
@@ -50,22 +51,21 @@ legalMoves :: Piece -> Position -> [Position]
 
 legalMoves Rook pos = let x = h pos
                           y = v pos
-                          vRange = map (consPosM x) $ map (\n -> n + y) fullRange
                           hRange = map (flip consPosM y) $ map (\n -> n + x) fullRange
-
+                          vRange = map (consPosM x) $ map (\n -> n + y) fullRange
                           --Get only on the grid moves, filter out the starting position.
                           moves = filter (\a -> a /= pos) $ catMaybes $ hRange ++ vRange
                       in moves
 
 legalMoves Queen pos = let  x = h pos
                             y = v pos
-                            hRange = map (consPosM x) $ map (\n -> n + y) fullRange
-                            vRange = map (flip consPosM y) $ map (\n -> n + x) fullRange
                             dRange = map (diagonalAdder x y) fullRange
+                            hRange = map (flip consPosM y) $ map (\n -> n + x) fullRange
+                            vRange = map (consPosM x) $ map (\n -> n + y) fullRange
 
                             --Get only on the grid moves (horizontal, vertical, diagonal),
                             --filter out starting position.
-                            moves = filter (\a -> a /= pos) $ catMaybes $ vRange ++ hRange ++ dRange
+                            moves = filter (\a -> a /= pos) $ catMaybes $ dRange ++ hRange ++ vRange
                        in moves
 
 legalMoves Bisshop pos = let x = h pos
@@ -81,7 +81,7 @@ legalMoves King pos = let x = h pos
                           vRange = map (flip consPosM y) $ map (\n -> n + x) range1
                           dRange = map (diagonalAdder x y) range1
                           --Get only on the grid moves, filter out the starting position.
-                          moves = filter (\a -> a /= pos) $ catMaybes $ hRange ++ vRange ++ dRange
+                          moves = filter (\a -> a /= pos) $ catMaybes $ dRange ++ hRange ++ vRange
                        in moves
 
 --Handling a start position for white.
@@ -90,3 +90,9 @@ legalMoves Pawn (Position x 2) = [Position x 3, Position x 4]
 legalMoves Pawn (Position x 7) = [Position x 6, Position x 5]
 --Handling all non starting positions.
 legalMoves Pawn (Position x y) = [Position x (y+1)]
+
+
+legalMoves Knight (Position x y) =  let hRange = map (\t -> [(fst t, snd t + 1), (fst t, snd t - 1)]) [(x + 2, y), (x - 2, y)]
+                                        vRange = map (\t -> [(fst t + 1, snd t), (fst t - 1, snd t)]) [(x, y + 2), (x, y - 2)]
+                                        tupleToPos t = consPosM (fst t) (snd t)
+                                    in (catMaybes $ map tupleToPos $ concat $ hRange ++ vRange)
