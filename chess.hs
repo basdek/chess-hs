bc34eaf4c97cea391dc167af1edb037c5d857c3d
Sebastiaan de Kwant <basdekwant@gmail.com>
2018-05-12 16:15:43 +0200

    Solve white and black pawn diff, cosmetic improv
    Black and white pawns move in opposite directions, to make this possible
    changed the signature of legalmoves to start from a PieceInstance
    instead of a piece.
    
    Also made the v and h functions obsolete: deconstructing Position in the
    implementation directly.

--- chess.hs
+++ chess.hs
@@ -1,6 +1,6 @@
 module Chess
   (
-    legalMoves,
+    legalMoves',
     consPos,
     PieceInstance
   ) where
@@ -15,9 +15,6 @@ data Piece = Rook | Bisshop | King | Queen | Knight | Pawn deriving (Show, Eq)
 data PieceInstance = PieceInstance Color Piece deriving (Show, Eq)
 
 data Position = Position Int Int deriving (Show, Eq, Ord)
-v (Position _ y) = y
-h (Position x _) = x
-
 
 constructablePos :: Int -> Int -> Bool
 constructablePos x y = 0 < x && x <= 8 && 0 < y && y <= 8
@@ -39,60 +36,41 @@ humanReadablePosition (Position x y) = let letters = ['A'..'H']
 diagonalAdder :: Int -> Int -> Int -> Maybe Position
 diagonalAdder x y n  =  consPosM (x + n) (y + n)
 
-fullRange = [(-8)..8]
+fullRange :: [Int]
+fullRange = filter (\n -> n > 0) [(-8)..8]
 
-data Board = Board
+legalMoves' (PieceInstance _ Rook) (Position x y) = let hRange = map (flip consPosM y) $ map (\n -> n + x) fullRange
+                                                        vRange = map (consPosM x) $ map (\n -> n + y) fullRange
+                                                        --Get only on the grid moves
+                                                        moves = catMaybes $ hRange ++ vRange
+                                                    in moves
 
-check :: Board -> Bool
-check _ = False
+legalMoves' (PieceInstance _ Queen) (Position x y) = let dRange = map (diagonalAdder x y) fullRange
+                                                         hRange = map (flip consPosM y) $ map (\n -> n + x) fullRange
+                                                         vRange = map (consPosM x) $ map (\n -> n + y) fullRange
+                                                         moves = catMaybes $ dRange ++ hRange ++ vRange
+                                                     in moves
 
+legalMoves' (PieceInstance _ Bisshop) (Position x y) = catMaybes $ map (diagonalAdder x y) fullRange
 
-legalMoves :: Piece -> Position -> [Position]
+legalMoves' (PieceInstance _ King) (Position x y) = let range1 = [-1, 1]
+                                                        dRange = map (diagonalAdder x y) range1
+                                                        hRange = map (consPosM x) $ map (\n -> n + y) range1
+                                                        vRange = map (flip consPosM y) $ map (\n -> n + x) range1
+                                                        moves = catMaybes $ dRange ++ hRange ++ vRange
+                                                    in moves
 
-legalMoves Rook pos = let x = h pos
-                          y = v pos
-                          hRange = map (flip consPosM y) $ map (\n -> n + x) fullRange
-                          vRange = map (consPosM x) $ map (\n -> n + y) fullRange
-                          --Get only on the grid moves, filter out the starting position.
-                          moves = filter (\a -> a /= pos) $ catMaybes $ hRange ++ vRange
-                      in moves
+legalMoves' (PieceInstance _ Knight) (Position x y) =  let hRange = map (\t -> [(fst t, snd t + 1), (fst t, snd t - 1)]) [(x + 2, y), (x - 2, y)]
+                                                           vRange = map (\t -> [(fst t + 1, snd t), (fst t - 1, snd t)]) [(x, y + 2), (x, y - 2)]
+                                                           tupleToPos t = consPosM (fst t) (snd t)
+                                                       in (catMaybes $ map tupleToPos $ concat $ hRange ++ vRange)
 
-legalMoves Queen pos = let  x = h pos
-                            y = v pos
-                            dRange = map (diagonalAdder x y) fullRange
-                            hRange = map (flip consPosM y) $ map (\n -> n + x) fullRange
-                            vRange = map (consPosM x) $ map (\n -> n + y) fullRange
-
-                            --Get only on the grid moves (horizontal, vertical, diagonal),
-                            --filter out starting position.
-                            moves = filter (\a -> a /= pos) $ catMaybes $ dRange ++ hRange ++ vRange
-                       in moves
-
-legalMoves Bisshop pos = let x = h pos
-                             y = v pos
-                             --Get only on the grid moves, filter out the starting position.
-                             moves = filter (\a -> a /= pos) $ catMaybes $ map (diagonalAdder x y) fullRange
-                         in moves
-
-legalMoves King pos = let x = h pos
-                          y = v pos
-                          range1 = [(-1)..1]
-                          hRange = map (consPosM x) $ map (\n -> n + y) range1
-                          vRange = map (flip consPosM y) $ map (\n -> n + x) range1
-                          dRange = map (diagonalAdder x y) range1
-                          --Get only on the grid moves, filter out the starting position.
-                          moves = filter (\a -> a /= pos) $ catMaybes $ dRange ++ hRange ++ vRange
-                       in moves
 
 --Handling a start position for white.
-legalMoves Pawn (Position x 2) = [Position x 3, Position x 4]
+legalMoves' (PieceInstance White Pawn) (Position x 2) = [Position x 3, Position x 4]
 --Handling a start position for black.
-legalMoves Pawn (Position x 7) = [Position x 6, Position x 5]
---Handling all non starting positions.
-legalMoves Pawn (Position x y) = [Position x (y+1)]
-
-
-legalMoves Knight (Position x y) =  let hRange = map (\t -> [(fst t, snd t + 1), (fst t, snd t - 1)]) [(x + 2, y), (x - 2, y)]
-                                        vRange = map (\t -> [(fst t + 1, snd t), (fst t - 1, snd t)]) [(x, y + 2), (x, y - 2)]
-                                        tupleToPos t = consPosM (fst t) (snd t)
-                                    in (catMaybes $ map tupleToPos $ concat $ hRange ++ vRange)
+legalMoves' (PieceInstance Black Pawn) (Position x 7) = [Position x 6, Position x 5]
+--Handling all non starting positions for white.
+legalMoves' (PieceInstance Black Pawn) (Position x y) = [Position x (y - 1)]
+--Handling all non starting positions for black.
+legalMoves' (PieceInstance White Pawn) (Position x y) = [Position x (y + 1)]
